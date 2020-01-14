0e0312a97d8837425d65aeef438cfb5fb4d05582
Luan Giovani <luangiovani@gmail.com>
2020-01-14 15:38:48 -0300

    version luan.github

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // var a = moment();
+    // var b = moment([2007, 1, 28]);
+    // var difference = a.diff(b, 'years') // 1
+
+    // $(".time-of-experience").html(difference);
+});
