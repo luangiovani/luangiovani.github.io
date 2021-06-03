071a3cf84da67be82e813fb114e3fd076bda46a2
Luan Giovani <luangiovani@gmail.com>
2021-06-03 19:37:28 +0100

    clean

--- js/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    // var a = moment();
-    // var b = moment([2007, 1, 28]);
-    // var difference = a.diff(b, 'years') // 1
-
-    // $(".time-of-experience").html(difference);
-});
